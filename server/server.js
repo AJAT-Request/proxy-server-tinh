c6159eb2bfe15e67dee8409359778506a64c853b
Tinh <tinhkle@Doxie.local>
2018-09-20 12:53:19 -0700

    added proxy server

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const proxy = require('http-proxy-middleware');
 const path = require('path');
 const app = express();
 const port = 3000;
 
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, '../public/')));
+app.use('/listings', proxy({target: 'http://localhost:3001', changeOrigin: true}))
 
 app.get('/rooms/:roomId', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
