ade1f83ee67c113421ab43902d53998421a44b41
Tinh <tinhkle@Doxie.local>
2018-09-22 16:58:30 -0700

    added process.env.PORT

--- server/server.js
+++ server/server.js
@@ -2,12 +2,12 @@ const express = require('express');
 const proxy = require('http-proxy-middleware');
 const path = require('path');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, '../public')));
-app.use('/listings', proxy({target: 'http://ec2-54-183-253-46.us-west-1.compute.amazonaws.com:3001'}));
-app.use('/reviews', proxy({target: 'http://localhost:3002'}));
-app.use('/bookingBox', proxy({target: 'http://localhost:3003'}));
+app.use('/listings', proxy({target: 'http://ec2-18-144-8-154.us-west-1.compute.amazonaws.com:3001'}));
+app.use('/reviews', proxy({target: 'http://ec2-13-57-210-179.us-west-1.compute.amazonaws.com:3002'}));
+app.use('/bookingBox', proxy({target: 'http://ec2-18-217-121-164.us-east-2.compute.amazonaws.com:3003'}));
 
 app.get('/rooms/:roomId', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
@@ -16,4 +16,5 @@ app.get('/rooms/:roomId', (req, res) => {
 app.listen(port, () => {
   console.log(`Listening on port: ${port}`);
 });
+
 
